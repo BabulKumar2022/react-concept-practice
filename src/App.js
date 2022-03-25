4c004b5acf0b8e6690b40ec59998326993721b35
BabulKumar2022 <bb3583400@gmail.com>
2022-03-25 14:36:19 +0600

    cartadded

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './Card/Card';
 import Navbar from './Navbar/Navbar';
+
+
 function App() {
   const [guns, setGuns] = useState([]);
-  console.log(guns);
+
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/mir-hussain/guns/main/data.json')
     .then(res => res.json())
     .then(data => setGuns(data));
   },[]);
   return (
-    <div className="App">
+    <div className="">
       <Navbar/>
+      <div className="card-container">
+      {
+        guns.map((gun) =>(
+          <Card key={gun.id} gunData={gun}/> 
+          ))
+      }
+      </div>
     </div>
   );
 }
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,8 +1,25 @@
 import React from 'react';
-
-const Card = () => {
+import './Card.css'
+import {BsFillCartFill} from "react-icons/bs";
+const Card = ({gunData}) => {
+    // console.log(props.gunData);
+    const {name, img, bullet, capacity, action, category, price} = gunData;
+    console.log(gunData);
     return (
-        <div>
+        <div className='card'>
+            <div className="image-container">
+                <img src={img} alt="" srcset=""/>
+            </div>
+            <h2>{name}</h2>
+            <p>Bulet Type: {bullet}</p>
+            <p>Capacity: {capacity}</p>
+            <p>Action: {action}</p>
+            <p>Category: {category}</p>
+           
+            <div className="add-to-cart">
+                <button> <BsFillCartFill className='icon'/></button>
+                <h1>Price: {price}</h1>
+            </div>
             
         </div>
     );
