abf60c9d6ee6b3530c2f59d4883d74be77d6f496
BabulKumar2022 <bb3583400@gmail.com>
2022-03-25 13:01:37 +0600

    first commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Navbar.css";
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <h1>It is navbar</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
